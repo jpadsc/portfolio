c2a9a976383494dc84d30b3591a27c89efdc125f
Javier Ponce <130936434+jpadsc@users.noreply.github.com>
2025-02-15 20:55:48 -0800

    Update global.js

--- global.js
+++ global.js
@@ -3,6 +3,6 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-
-let currentLink = ($$("nav a")).find((a) => a.host === location.host && a.pathname === location.pathname);
+let navLinks = $$("nav a");
+let currentLink = (navLinks).find((a) => a.host === location.host && a.pathname === location.pathname);
 currentLink?.classList.add('current');
